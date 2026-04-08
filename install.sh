823706fd6932c3afd8d74ee47a9a6205f6758dbd
MDiaznf23 <muhammaddiaznurfarizki@gmail.com>
2026-04-08 15:12:39 +0700

    fix dynamic width in topbar also fix missing dependency in install.sh

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ mkdir -p $BACKUP_DIR
 echo "Installing system packages..."
 sudo pacman -S --needed --noconfirm \
     i3-wm i3status alacritty pcmanfm rofi picom feh scrot xclip xdotool dex \
-    brightnessctl firefox dolphin playerctl lm_sensors imagemagick xsettingsd \
+    brightnessctl firefox dolphin xorg-xdpyinfo playerctl lm_sensors imagemagick xsettingsd \
     python python-pip python-pipx lxsession fish redshift inotify-tools\
     jq bc dunst rsync fastfetch pamixer python-i3ipc qt5ct cava tex-gyre-fonts 
 
